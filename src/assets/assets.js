50c3b4c30d0ccca2c876fa58e396f553e45c3936
1410git <prakashraj2020.pk@gamil.com>
2025-09-17 05:45:12 +0530

    Add all assets including login-bg

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,6 @@
 import logo from './logo.png';
 import upload from './upload.png';
-import login  from './login-bg.png'
+import login  from './Login-bg.png'
 import profile from'./profile.png'
 import device from './device.jpg';
 
